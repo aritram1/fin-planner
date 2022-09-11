15ecaa9e58fd90ed2c2cd2c2f74700be2f8c2415
aritram1 <aritram1@gmail.com>
2022-09-12 02:20:04 +0530

    Interim commit

--- FinancialPlanner/force-app/main/default/lwc/fpShowTables/fpShowTables.js
+++ FinancialPlanner/force-app/main/default/lwc/fpShowTables/fpShowTables.js
@@ -1,67 +1,88 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from "lwc";
 //import getContentInString from '@salesforce/apex/FP_FinancialPlannerController.getContentInString';
-import ChartJS from '@salesforce/resourceUrl/ChartJS';
-import { loadScript } from 'lightning/platformResourceLoader';
-
-
+import ChartJS from "@salesforce/resourceUrl/ChartJS";
+import { loadScript } from "lightning/platformResourceLoader";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 export default class FpShowTables extends LightningElement {
-    chartJsInitialized = false; 
-    chart;
-    config={
-        type : 'doughnut',
-        data :{
-            datasets :[
-                {
-                    data: [],
-                    backgroundColor :[
-                        'rgb(255,99,132)',
-                        'rgb(255,159,64)',
-                        'rgb(255,205,86)',
-                        'rgb(75,192,192)',
-                    ],
-                    label:'Dataset 1'
-                }
-            ],
-            labels:[]
-        },
-        options: {
-            responsive : true,
-            legend : {
-                position :'right'
-            },
-            animation:{
-                animateScale: true,
-                animateRotate : true
-            }
+  // Chart Variables
+  chart;
+  chartJsInitialized = false;
+  chartData = [1, 2, 3, 4];
+  chartBackgroundColors = ["red", "blue", "green", "yellow"];
+  chartType = "doughnut";
+  chartConfig = {
+    type: this.chartType,
+    data: {
+      datasets: [
+        {
+          data: [1, 2, 3, 4], //this.chartData,
+          backgroundColor: ["red", "blue", "green", "yellow"], //this.chartBackgroundColors,
+          label: "Dataset 1"
         }
-    };
-
-    durationValue;
-    durationOptions = [{label: 'Weekly', value:'Weekly'}, {label: 'Monthly', value: 'Monthly'}];
-    handleDurationChange(e){
-        console.log(this.durationValue);
-        
+      ],
+      labels: []
+    },
+    options: {
+      responsive: false,
+      legend: {
+        position: "right"
+      },
+      animation: {
+        animateScale: true,
+        animateRotate: true
+      }
     }
-    
+  };
 
-    renderedCallback(){
-        if(this.chartJsInitialized) return;
-        Promise.all([
-            loadScript(this, ChartJS)
-        ])
-        .then(() => {
-            const ctx = this.template.querySelector('canvas.donut').getContext('2d');
-            this.chart = new window.Chart(ctx, this.config);
-            this.isChartJsInitialized = true;
-        })
-        .catch(error => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error loading Chart',
-                    message: error.message,
-                    variant: 'error',
-                })
-            );
-        });
-    }
+  // Duration Variables
+  durationValue;
+  durationOptions = [
+    { label: "Weekly", value: "Weekly" },
+    { label: "Monthly", value: "Monthly" }
+  ];
+  handleDurationChange(e) {
+    this.durationValue = e.detail.value;
+    console.log(this.durationValue);
+    //this.modifyData(this.durationValue, this.chartData);
+  }
+
+  modifyData() {
+    // switch (this.durationValue) {
+    //   case "Weekly":
+    //     this.chartData = this.chartData.forEach((e) => e + 1);
+    //     break;
+    //   case "Monthly":
+    //     this.chartData = this.chartData.reverse();
+    //     break;
+    //   default:
+    //     this.chartData = [];
+    //     break;
+    // }
+  }
+
+  // renderedCallback
+  renderedCallback() {
+    //If chart is initialized, do nothing
+    if (this.chartJsInitialized) return;
+
+    Promise.all([loadScript(this, ChartJS)])
+      .then(() => {
+        const ctx = this.template
+          .querySelector("canvas.chart")
+          .getContext("2d");
+        this.chart = new window.Chart(ctx, this.config);
+        this.isChartJsInitialized = true;
+        console.dir(this.chart);
+        console.log("loaded");
+      })
+      .catch((error) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error loading Chart",
+            message: error.message,
+            variant: "error"
+          })
+        );
+      });
+  }
 }
